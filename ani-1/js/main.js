21f9b18e039dcab39b5b04a2fee81de86b09fa60
Manu <90511900+Manumanoharan@users.noreply.github.com>
2022-09-01 12:17:46 +0530

    Add files via upload

---
+++ ani-1/js/main.js
@@ -0,0 +1,23 @@
+function startAD(){
+    setTimeout(function(){
+        document.getElementById('dots-1').style.backgroundColor="red"
+    }, 200)
+    setTimeout(function(){
+        document.getElementById('dots-2').style.backgroundColor="red"
+    }, 700)
+    setTimeout(function(){
+        document.getElementById('dots-3').style.backgroundColor="red"
+    }, 1200) 
+    setTimeout(function(){
+        document.getElementById('dots-4').style.backgroundColor="red"
+    }, 1700)
+    setTimeout(function(){
+        document.getElementById('dots-5').style.backgroundColor="red"
+    }, 2200)
+     setTimeout(function(){
+        document.getElementById('dots-6').style.backgroundColor="red"
+    }, 2700)
+    
+}
+
+window.load=startAD();
